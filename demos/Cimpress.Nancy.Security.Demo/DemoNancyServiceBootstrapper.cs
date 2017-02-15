e0377c897f77d1121384e4226e5f5e4e8bed2dba
jnallard <joshuaal2000@gmail.com>
2017-02-15 15:13:18 -0500

    Adds security example

---
+++ demos/Cimpress.Nancy.Security.Demo/DemoNancyServiceBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Cimpress.Nancy.Components;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Cimpress.Nancy.Config;
+
+namespace Cimpress.Nancy.Security.Demo
+{
+    public class DemoNancyServiceBootstrapper : NancyServiceBootstrapper
+    {
+        public DemoNancyServiceBootstrapper() : base()
+        {
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            IConfiguration configuration = new Configuration();
+            configuration.OptionalParameters = new Dictionary<string, string>
+            {
+                ["OAuth2Issuer"] = "[Pass root url of issuer here]",
+                ["OAuth2SecretKey"] = "[Pass application's secret key here]",
+                ["OAuth2ClientId"] = "[Pass application's client id here]"
+            };
+            container.Register(configuration);
+            base.ApplicationStartup(container, pipelines);
+        }
+    }
+}
---
+++ demos/Cimpress.Nancy.Security.Demo/Security/AuthValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cimpress.Nancy.Components;
+using Cimpress.Nancy.Config;
+
+namespace Cimpress.Nancy.Security.Demo.Security
+{
+    public class AuthValidator : OAuth2Validator
+    {
+        public AuthValidator(INancyLogger log, IConfiguration config) : base(log)
+        {
+            OAuth2Issuer = config.OptionalParameters["OAuth2Issuer"];
+            OAuthSecretKey = config.OptionalParameters["OAuth2SecretKey"];
+            OAuth2ClientId = config.OptionalParameters["OAuth2ClientId"];
+        }
+
+        public override string OAuth2Issuer { get; }
+        public override string OAuthSecretKey { get; }
+        public override string OAuth2ClientId { get; }
+    }
+}
