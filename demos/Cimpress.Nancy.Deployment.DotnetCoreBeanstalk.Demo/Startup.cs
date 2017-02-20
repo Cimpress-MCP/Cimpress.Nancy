e69663182fcefe843bf58d341253e121f29c9a17
Ciaran Downey <cdowney@cimpress.com>
2017-02-20 11:53:16 -0500

    Add example for deploying a .Net Core service to Elastic Beanstalk

---
+++ demos/Cimpress.Nancy.Deployment.DotnetCoreBeanstalk.Demo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cimpress.Nancy.Demo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nancy.Owin;
+
+namespace Cimpress.Nancy.Deployment.DotnetCoreBeanstalk.Demo
+{
+    public class Startup : Cimpress.Nancy.Demo.Startup
+    {
+        public Startup(IHostingEnvironment env) : base(env)
+        {
+        }
+    }
+}
