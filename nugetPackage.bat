4a852ebb28eb5bab18cc3bb20ad39b551c2c18d3
jnallard <joshuaal2000@gmail.com>
2017-05-15 10:04:35 -0400

    Updates nuget packing

--- nugetPackage.bat
+++ nugetPackage.bat
@@ -1,10 +1,6 @@
 echo %APPVEYOR_BUILD_NUMBER%
-powershell -Command "(gc src/Cimpress.Nancy/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Cimpress.Nancy/project.json"
-powershell -Command "(gc src/Cimpress.Nancy.Logging/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Cimpress.Nancy.Logging/project.json"
-powershell -Command "(gc src/Cimpress.Nancy.Security/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Cimpress.Nancy.Security/project.json"
-powershell -Command "(gc src/Cimpress.Nancy.Swagger/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Cimpress.Nancy.Swagger/project.json"
 
-dotnet pack src/Cimpress.Nancy --configuration Release --output src/Cimpress.Nancy/NuGet --version-suffix "alpha"
-dotnet pack src/Cimpress.Nancy.Logging --configuration Release --output src/Cimpress.Nancy.Logging/NuGet --version-suffix "alpha"
-dotnet pack src/Cimpress.Nancy.Security --configuration Release --output src/Cimpress.Nancy.Security/NuGet --version-suffix "alpha"
-dotnet pack src/Cimpress.Nancy.Swagger --configuration Release --output src/Cimpress.Nancy.Swagger/NuGet --version-suffix "alpha"
+dotnet pack src/Cimpress.Nancy --configuration Release --version-suffix "alpha"
+dotnet pack src/Cimpress.Nancy.Logging --configuration Release --version-suffix "alpha"
+dotnet pack src/Cimpress.Nancy.Security --configuration Release --version-suffix "alpha"
+dotnet pack src/Cimpress.Nancy.Swagger --configuration Release --version-suffix "alpha"
