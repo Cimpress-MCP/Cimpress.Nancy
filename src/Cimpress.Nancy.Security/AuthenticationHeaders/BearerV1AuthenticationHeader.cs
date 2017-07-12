59dbdce81a661bb3174d24ac026091198e9a6c9a
jnallard <joshuaal2000@gmail.com>
2017-07-12 13:08:35 -0400

    Adds classes to generate auth0 challenge headers

---
+++ src/Cimpress.Nancy.Security/AuthenticationHeaders/BearerV1AuthenticationHeader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Cimpress.Nancy.Security.AuthenticationHeaders
+{
+    public class BearerV1AuthenticationHeader : IAuthenticationHeader
+    {
+        private readonly string _realm;
+        private readonly string _clientId;
+        private readonly string _service;
+
+        public BearerV1AuthenticationHeader(string realm, string clientId, string service = null)
+        {
+            _realm = realm;
+            _clientId = clientId;
+            _service = service;
+        }
+
+        public void AddHeader(Response response, Request request)
+        {
+            var service = _service;
+            if (string.IsNullOrEmpty(_service))
+            {
+                service = $"{request.Url.SiteBase}";
+            }
+            var header = $"Bearer realm=\"{_realm}\", scope=\"client_id={_clientId} service={service}\"";
+            response.WithHeader("WWW-Authenticate", header);
+        }
+    }
+}
