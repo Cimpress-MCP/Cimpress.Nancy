9c3c211c5796fe8d63857d414eb24b9eadca0220
Jon Goodnow <jgoodnow@vistaprint.com>
2018-10-17 15:46:11 -0400

    Revert "Declare memory streams in using blocks to ensure they are properly disposed."
    This reverts commit cc2f404d1fd73ef2b420624eb79b0535f08b9cc9.

--- src/Cimpress.Nancy.Logging/LoggingBootstrapperExtender.cs
+++ src/Cimpress.Nancy.Logging/LoggingBootstrapperExtender.cs
@@ -57,18 +57,11 @@ namespace Cimpress.Nancy.Logging
             {
                 correlationId = (string) context.Items[CorrelationIdString];
             }
-
-            string responseBody;
-            using (var stream = new MemoryStream())
-            {
-                response.Contents(stream);
-                stream.Position = 0;
-                using (var streamReader = new StreamReader(stream))
-                {
-                    responseBody = streamReader.ReadToEnd();
-                }
-            }
-
+            
+            var stream = new MemoryStream();
+            response.Contents(stream);
+            stream.Position = 0;
+            var responseBody = new StreamReader(stream).ReadToEnd();
             var responseIsJson = response.ContentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(responseBody);
             var bodyObject = responseIsJson ? JsonConvert.DeserializeObject(responseBody) : responseBody;
 
@@ -102,11 +95,7 @@ namespace Cimpress.Nancy.Logging
 
             var formString = JsonConvert.SerializeObject(request.Form.ToDictionary());
             request.Body.Position = 0;
-            string bodyString;
-            using (var streamReader = new StreamReader(request.Body))
-            {
-                bodyString = streamReader.ReadToEnd();
-            }
+            var bodyString = new StreamReader(request.Body).ReadToEnd();
             request.Body.Position = 0;
 
             var isBodyJson = string.Equals(request.Headers.ContentType ?? string.Empty, "application/json", StringComparison.OrdinalIgnoreCase);
